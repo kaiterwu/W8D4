6274eaf18e2b39d11fc8d7ade2b452697e36fd54
Kaiter Wu <wukaiter@gmail.com>
2023-03-02 15:25:18 -0500

    working on clock implementation

--- clock.js
+++ clock.js
@@ -11,7 +11,15 @@ new Date()
 class Clock {
     constructer() {
         let thisdate = Date().toString()
-        let hms = thisdate.split("T")[1].split(":") //[hh,mm,ss]
+        let hms = thisdate.split(" ")[4].split(":") //[hh,mm,ss]
+        this.hours = hms[0]
+        this.minutes = hms[1]
+        this.seconds = hms[2]
+         
+            printTime()
+            setinterval(_tick,1000)
+           
+    
         
     }
 
@@ -19,22 +27,32 @@ class Clock {
 
 
     printTime() {
-
-
-    }
+        console.log(`${this.hours}:${this.minutes}:${this.seconds}`)
+     }
 
     _tick() {
-
-
+        this.seconds++ 
+        if (this.seconds === 60) {
+            this.minutes ++ 
+            this.seconds = 0 
+        }
+        if (this.minutes ===60) {
+            this.hours ++ 
+            this.minutes == 0 
+        }
+        if (this.hours === 24 ) {
+            this.hours =0 
+        }
+        return printTime()
     }
 
 
 
 }
 
-const clock = new Clock();
+const clock = new Clock();  
 
-//  today.split("T")[1].split(":")
+// //  today.split("T")[1].split(":")
 
-let thisdate = Date()
-let hms = thisdate.split("T")[1].split(":") //[hh,mm,ss]
+// let thisdate = Date()
+// let hms = thisdate.split("T")[1].split(":") //[hh,mm,ss]
